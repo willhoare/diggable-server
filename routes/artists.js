04dd1b9b35db87b2b74d9a000da644d6f58d9223
willhoare <williamhoare@gmail.com>
2023-02-04 18:50:53 -0500

    get and post requests for artists are working

--- routes/artists.js
+++ routes/artists.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
+const { v4: uuid } = require("uuid");
 
 const artists = require("../data/artist-details.json");
 
@@ -32,5 +33,44 @@ function getArist() {
   const artistData = fs.readFileSync("./data/artist-details.json");
   return JSON.parse(artistData);
 }
+
+router.post("/", (req, res) => {
+  const newVideoRead = fs.readFileSync("./data/artist-details.json");
+  const allArtists = JSON.parse(newVideoRead);
+
+  const { firstname, lastname, artistname, username, city, country } = req.body;
+  const newArtist = {
+    id: uuid(),
+    firstname: firstname,
+    lastname: lastname,
+    artistname: artistname,
+    image: "http://localhost:8080/images/happy-monday.jpeg",
+    username: username,
+    city: city,
+    country: country,
+
+    campaigns: [
+      {
+        id: "35bba08b-1b51-4153-ba7e-6da76b5ec1b9",
+        title: "North American Tour 2023",
+        description:
+          "The Happy Mondays are looking to tour their new album across Narth America in 2023 and are seeking support to help achieve their goal.",
+        goal: 0,
+        tourdates: 1628522461000,
+        rewards: [
+          {
+            1: "Personalised Video message",
+            2: "Signed T-shirt",
+            3: "Ticket for location of your choice",
+          },
+        ],
+      },
+    ],
+  };
+
+  allArtists.push(newArtist);
+  fs.writeFileSync("./data/artist-details.json", JSON.stringify(allArtists));
+  res.status(201).send("New artist profile created");
+});
 
 module.exports = router;
